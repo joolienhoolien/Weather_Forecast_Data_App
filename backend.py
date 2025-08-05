2e7a19c16bbc317721e6b982d6937dd7fb896540
joolienhoolien <julienpecquet@gmail.com>
2025-08-05 15:37:04 -0600

    Finishing up

--- backend.py
+++ backend.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "91e91e6d45f0d25c57a5de6ea339281b"
 
-def get_data(place, forecast_days=None, kind=None):
+def get_data(place, forecast_days=None):
     url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}&units=imperial"
     response = requests.get(url)
     data = response.json()
@@ -13,10 +13,6 @@ def get_data(place, forecast_days=None, kind=None):
 
     #From that limited data, return...
     dates = [(data[i]['dt_txt']) for i in range(len(data))]
-    if kind.lower() == "sky":
-        data = [(data[i]['weather'][0]['main']) for i in range(len(data))]
-    elif kind.lower() == "temperature":
-        data = [(data[i]['main']['temp']) for i in range(len(data))]
 
     return  dates, data
 
--- main.py
+++ main.py
@@ -8,10 +8,27 @@ days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select number of forecasted days")
 option = st.selectbox("Select data to view", ("Temperature", "Sky"))
 
-st.subheader(f"{option} for the next {days} days in {place}")
+try:
+    if place:
+        st.subheader(f"{option} for the next {days} days in {place}")
+        dates, data = get_data(place, days)
+        if option == "Temperature":
+            data = [(data[i]['main']['temp']) for i in range(len(data))]
 
-dates, temperatures = get_data(place, days, option)
-print(dates, temperatures)
-
-figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature"})
-st.plotly_chart(figure)
+            figure = px.line(x=dates, y=data, labels={"x": "Date", "y": "Temperature"})
+            st.plotly_chart(figure)
+        elif option == "Sky":
+            data = [(data[i]['weather'][0]['main']) for i in range(len(data))]
+            image_paths = []
+            for sky in data:
+                if sky == "Clear":
+                    image_paths.append("images/clear.png")
+                elif sky == "Rain":
+                    image_paths.append("images/rain.png")
+                elif sky == "Snow":
+                    image_paths.append("images/snow.png")
+                elif sky == "Cloud":
+                    image_paths.append("images/cloud.png")
+            st.image(image_paths, width=80)
+except KeyError as error:
+    st.write("We couldn't get that information. Check to see if the place is spelled correctly!")
